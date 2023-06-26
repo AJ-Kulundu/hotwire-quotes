bed2f7afdc10e699575aca06948cfcf99a2507bd
AJ-Kulundu <jameskulundu@gmail.com>
2023-06-26 21:56:27 +0300

    Adding an update route to the line_item_controller

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,5 +1,6 @@
 class LineItemDatesController < ApplicationController
     before_action :set_quote
+    before_action :set_line_item_date, only: [:edit, :update, :destroy]
   
     def new
       @line_item_date = @quote.line_item_dates.build
@@ -14,7 +15,17 @@ class LineItemDatesController < ApplicationController
         render :new, status: :unprocessable_entity
       end
     end
-  
+
+    def edit; end
+
+    def update
+      if @line_item_date.update(line_item_date_params)
+        redirect_to @quote, notice: "Date was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+    
     private
   
     def line_item_date_params
@@ -23,5 +34,9 @@ class LineItemDatesController < ApplicationController
   
     def set_quote
       @quote = current_company.quotes.find(params[:quote_id])
+    end
+
+    def set_line_item_date
+        @line_item_date = @quote.line_item_dates.find(params[:id])
     end
   end
